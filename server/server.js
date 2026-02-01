9632b852ca6fbd07dde40ff587b30c3ce7e58130
nicolodo <nicolasbernard19@gmail.com>
2026-02-01 17:34:22 +0000

    fix db connection string

--- server/server.js
+++ server/server.js
@@ -11,14 +11,43 @@ app.use(express.json());
 app.use(cors);
 dotenv.config();
 
-const db = new pg.Pool({ // idk what a Pool is but that pg is postgres (related to SQL)
-    connectionString: process.env.DB_CONN // our env var DB_CONN connects w/ idk supa or re-something
+
+const db = new pg.Pool({
+    connectionString: process.env.DB_CONN_STRING,
+})
+
+db.query(
+    `INSERT INTO messages (msg_name, content) VALUES ('hello there','general kenobi')`
+);
+
+// db.query(`SELECT * FROM USERS WHERE id = $1`, [req.body.user_id]);
+
+db.query(`INSERT INTO messages (msg_name, content) VALUES ($1, $2)`, [
+    "Does anyone else..",
+    "love oranges",
+]);
+db.query(`INSERT INTO messages (msg_name, content) VALUES ($1, $2)`, [
+    "hot take:",
+    "I think oranges suck",
+]);
+
+// making a listen port so I can see what's happening
+const PORT = 8080
+app.listen(PORT, () => {
+    console.log("something listens at http://localhost:"+String(PORT));
+})
+
+//  making an async function as the endpoint
+// This logs the table to the console
+app.get("/messages", async function (request, response) {
+    const messages = await db.query("SELECT * FROM messages");
+    response.json(messages.rows);
+});
+app.get("/messages", async function (request, response) {
+    const messages = await db.query("SELECT * FROM messages WHERE id = 1");
+    response.json(messages.rows);
 })
 
 app.get('/', (req, res) => {
     res.send('Hello!');
-})
-
-app.listen('8080', () => {
-    console.log('server started on port http://localhost:8080');
 })
